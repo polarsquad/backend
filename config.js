76fc3d10b1aedf7d065e25303d247b448585db6f
rhotep <andreas.pittrich@posteo.de>
2017-10-23 04:55:46 +0200

    config

---
+++ config.js
@@ -0,0 +1,6 @@
+var fs 			= require('fs'),
+	config		= JSON.parse(fs.readFileSync('config/config.json')),
+	key_path	= (process.argv[2] || '' ).split('.'),
+	result		= ''
+
+console.log(key_path.reduce( (obj, part) => obj && obj[part] || undefined, config))
