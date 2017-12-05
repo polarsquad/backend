16814b043630ad5d2b8e8ea5386bf37f8083446b
rhotep <andreas.pittrich@posteo.de>
2017-12-05 02:08:12 +0100

    added csv export

---
+++ dpd/resources/actions/exportitems.js
@@ -0,0 +1,25 @@
+var query   	= ctx.query,
+    format  	= query.format.toUpperCase()    || 'JSON',
+    keys    	= query.keys.split(',')         || [],
+    response	= this
+
+function sanetizePropertiy(prop){
+    if(typeof prop != 'string') prop = JSON.stringify(prop)
+    return prop.replace(",", "\\COMMA")
+}
+
+
+$addCallback()
+
+dpd.items.get({})
+.then( items => {
+    if(format =='CSV'){
+        ctx.res.setHeader('Content-Type', 'text/csv')
+        return      keys.join(',')+'\n'
+                   +items.map( item => keys.map( key => sanetizePropertiy(item[key])).join(',')).join('\n')
+    }
+    ctx.res.setHeader('Content-Type', 'application/json')   
+    return  JSON.stringify(items.map( item => keys.reduce( (result, key) => {result[key] = item[key]; return result}, {})))
+})
+.then( result =>  ctx.res.end(result))
+
