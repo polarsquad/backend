e93171c1c9514fe38c819e2ba8cf0d5e73d56d64
rhotep <andreas.pittrich@posteo.de>
2018-11-23 20:55:27 +0100

    tiles

--- dpd/checkJsonData.js
+++ dpd/checkJsonData.js
@@ -2,12 +2,13 @@
 var	fs				= require('fs'),
 	icItemConfig	= require (process.cwd()+'/public/ic-item-config.js'),
 	json_file		= process.argv[2] || console.error('missing JSON file'),
-	data			= json_file && JSON.parse(fs.readFileSync(process.argv[2], 'utf8'))
+	data			= json_file && JSON.parse(fs.readFileSync(process.argv[2], 'utf8')),
+	valid_data		= []
 
 console.log('Nuber of items:'+ data.length)
 
 data.forEach( (item, index) => {
-	icItemConfig.properties.forEach(property => {
+	if(icItemConfig.properties.every(property => {
         var e = property.getErrors(item[property.name])
         if(e) {
         	console.log('-------------------')
@@ -16,6 +17,11 @@ data.forEach( (item, index) => {
         	console.log(e)
         	console.log(item[property.name])
         	console.log('-------------------')
+        	return false
         }
-    })
+        return true
+    }))  valid_data.push(item)
 })
+
+
+if(process.argv[2]) fs.writeFileSync('valid_data.json', JSON.stringify(valid_data, null, 4), 'utf8')
