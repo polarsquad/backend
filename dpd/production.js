e3e80e44c505703c885d911d10c7e1a20795fa94
rhotep <andreas.pittrich@posteo.de>
2018-12-23 06:22:58 +0100

    translate on start

--- dpd/production.js
+++ dpd/production.js
@@ -2,27 +2,32 @@
 
 process.chdir(__dirname);
 
-var	config	= 	JSON.parse(require('fs').readFileSync('../config/config.json', 'utf8')),
-	deployd	= 	require('deployd'),
-	server 	= 	deployd({
-					port:	config.port,
-					env: 	'production',
-					db: {
-						host: 	config.db.host,
-						port: 	config.db.port,
-						name: 	config.db.name,
-						credentials: {
-							username: config.db.credentials.username,
-							password: config.db.credentials.password
-						}
-					}
-				})
-
+var	config			= 	JSON.parse(require('fs').readFileSync('../config/config.json', 'utf8')),
+	deployd			= 	require('deployd'),
+	server 			= 	deployd({
+							port:	config.port,
+							env: 	'production',
+							db: {
+								host: 	config.db.host,
+								port: 	config.db.port,
+								name: 	config.db.name,
+								credentials: {
+									username: config.db.credentials.username,
+									password: config.db.credentials.password
+								}
+							}
+						}),
+	internalClient = require('deployd/lib/internal-client');
 
 server.listen()
 
 server.on('listening', function() {
-	console.log("Server is listening")
+	console.log("Server is listening on "+ config.port)
+
+	var dpd = internalClient.build(server)
+
+	dpd.actions.post('updateTranslations')
+	.then(console.log, console.log)
 })
 
 server.on('error', function(err) {
