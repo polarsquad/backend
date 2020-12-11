b036962f415ecc25eb98579a8d5be9cb18bde766
rhotep <andreas.pittrich@posteo.de>
2020-12-11 17:20:56 +0100

    filter export by tags

--- dpd/resources/actions/exportitems.js
+++ dpd/resources/actions/exportitems.js
@@ -1,12 +1,13 @@
 var query   	= ctx.query,
-    format  	= (query.format    || 'json').toUpperCase(),
-    keys    	= (query.keys      || 'title').split(',')
-    response	= this
+	format  	= (query.format    || 'json').toUpperCase(),
+	keys    	= (query.keys      || 'title').split(',')
+	tags        = (query.tags      || 'title').split(',')
+	response	= this
 
 function sanetizePropertiy(prop){
-    if(!prop) return ""
-    if(typeof prop != 'string') prop = JSON.stringify(prop)
-    return prop.replace(",", "\\COMMA")
+	if(!prop) return ""
+	if(typeof prop != 'string') prop = JSON.stringify(prop)
+	return prop.replace(",", "\\COMMA")
 }
 
 $addCallback()
@@ -15,18 +16,24 @@ $addCallback()
 dpd.items.get({})
 .then( items => {
 
-    items.forEach( item => {
-        item.link = icConfig.frontendUrl+'/item/'+item.id
-    })
+	if(tags && tags.length > 0){
+		items =	items.filter( function(item){
+					return item.tags.some( function(tag){ return tags.includes(tag) } )
+				})
+	}
 
-    if(format =='CSV'){
-        ctx.res.setHeader('Content-Type', 'text/csv; charset=utf-8')
-        ctx.res.setHeader('content-disposition', "attachment; filename=\"" + (icUtils.config.title||'ic').toLowerCase()+"_items.csv\"")
-        return      keys.join(',')+'\n'
-                   +items.map( item => keys.map( key => sanetizePropertiy(item[key])).join(',')).join('\n')
-    }
-    ctx.res.setHeader('Content-Type', 'application/json')   
-    return  JSON.stringify(items.map( item => keys.reduce( (result, key) => {result[key] = item[key]; return result}, {})))
+	items.forEach( item => {
+		item.link = icConfig.frontendUrl+'/item/'+item.id
+	})
+
+	if(format =='CSV'){
+		ctx.res.setHeader('Content-Type', 'text/csv; charset=utf-8')
+		ctx.res.setHeader('content-disposition', "attachment; filename=\"" + (icUtils.config.title||'ic').toLowerCase()+"_items.csv\"")
+		return      keys.join(',')+'\n'
+				   +items.map( item => keys.map( key => sanetizePropertiy(item[key])).join(',')).join('\n')
+	}
+	ctx.res.setHeader('Content-Type', 'application/json')   
+	return  JSON.stringify(items.map( item => keys.reduce( (result, key) => {result[key] = item[key]; return result}, {})))
 })
 .then( result =>  ctx.res.end(result))
 
