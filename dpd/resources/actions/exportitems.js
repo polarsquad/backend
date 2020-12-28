bb6d2dae5190d4b0eca330557110bc3ab2a4d2dc
rhotep <andreas.pittrich@posteo.de>
2020-12-28 19:19:10 +0100

    fix exportitems

--- dpd/resources/actions/exportitems.js
+++ dpd/resources/actions/exportitems.js
@@ -1,7 +1,7 @@
 var query   	= ctx.query,
 	format  	= (query.format    || 'json').toUpperCase(),
-	keys    	= (query.keys      || 'title').split(',')
-	tags        = query.tags ?	query.tags.split(',') : []
+	keys    	= (query.keys      || 'title').split(','),
+	tags        = query.tags ?	query.tags.split(',') : [],
 	response	= this
 
 function sanetizePropertiy(prop){
