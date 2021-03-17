70f109322689228ff472d8ff8b77f805ef1cdb55
rhotep <andreas.pittrich@posteo.de>
2021-03-17 21:26:19 +0100

    debug public_items

--- public_api/items.js
+++ public_api/items.js
@@ -126,7 +126,7 @@ export async function invokeImportScript(key, config){
 
 export function mergeResults(results){
 
-	console.log('Results:', results.key, results.status, result.message, results.items.legth)
+	console.log('Results:', results.key, results.status, results.message, results.items.legth)
 
 	return 	{
 				results: 	results
