0a3b482673118f499fc4c10e0c4bf84b1272ee66
rhotep <andreas.pittrich@posteo.de>
2021-04-12 21:04:22 +0200

    import pa

--- dpd/production.js
+++ dpd/production.js
@@ -65,9 +65,7 @@ async function importJSON(dpd, import_json, clear_before_import){
 		const current_items = await dpd.items.get()
 
 
-		await 	Promise.all(current_items.map( item => {
-					dpd.del(item.id)
-				}))
+		await 	Promise.all(current_items.map( item => dpd.del(item.id) ))
 		
 		console.log('Clearing items [done]')			
 	}
