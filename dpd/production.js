6bf68418d9a131f15b521a74b4fe2e4f5ae00c3f
rhotep <andreas.pittrich@posteo.de>
2021-06-12 06:32:40 +0200

    import items

--- dpd/production.js
+++ dpd/production.js
@@ -97,7 +97,7 @@ async function importJSON(dpd, import_json, clear_before_import){
 					const ref = items.find( i => i.title == item.location_ref)
 
 					if(!ref) console.log('Bad location_ref ', item.location_ref)
-						
+					console.log(ref.id)	
 					return dpd.items.put(item.id, {location_ref: ref.id })
 				}
 
