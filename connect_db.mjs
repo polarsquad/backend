03175d240e524183a10417a833d41dc631383786
ffafi <firas.fafi@polarsquad.com>
2022-05-31 12:57:47 +0200

    removal of hardcoded localhost + adding host env_var for DB config

--- connect_db.mjs
+++ connect_db.mjs
@@ -2,15 +2,16 @@ import	{ default as mongodb 	}	from 'mongodb'
 
 const 	{ MongoClient } = 	mongodb
 
-export async function getLocalDB(port, name, user, pass){
+export async function getLocalDB(host, port, name, user, pass){
 
+	if(!host) throw "getLocalDB: missing host"
 	if(!port) throw "getLocalDB: missing port"
 	if(!name) throw "getLocalDB: missing name"
 	if(!user) throw "getLocalDB: missing user"
 	if(!pass) throw "getLocalDB: missing pass"
 
 
-	const 	connect_str	= `mongodb://${user}:${pass}@127.0.0.1:${port}/${name}`
+	const 	connect_str	= `mongodb://${user}:${pass}@${host}:${port}/${name}`
 
 	process.stdout.write('\nConnecting to: '+connect_str+' ... ')
 
