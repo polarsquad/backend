0fea7c4b1a667adae693b1e64b441a440fe95d6e
Patrick Da Silva <patrick.dasilva@polarsquad.com>
2022-07-17 22:37:50 +0200

    Removed "--****" default values in config/index.js

--- config/index.js
+++ config/index.js
@@ -8,42 +8,42 @@ const booleanEnvVar = (env_variable, default_value) => (
 )
 
 exports.config = {
-    title:                        process.env.TITLE || "--title",
-    translationSpreadsheetUrl:    process.env.TRANSLATION_SPREADSHEET_URL || "--translation-spreadsheet-URL",
-    googleTranslateApiKey:        process.env.GOOGLE_TRANSLATE_API_KEY || "--google-translate-api-key",
-    deepLApiKey:                  process.env.DEEP_L_API_KEY || "--deepL-api-key",
-    frontendUrl:                  process.env.FRONTEND_URL || "--frontend-URL",
-    port:                         process.env.PRODUCTION_API_PORT || "--production-api-port",
+    title:                        process.env.TITLE,
+    translationSpreadsheetUrl:    process.env.TRANSLATION_SPREADSHEET_URL,
+    googleTranslateApiKey:        process.env.GOOGLE_TRANSLATE_API_KEY,
+    deepLApiKey:                  process.env.DEEP_L_API_KEY,
+    frontendUrl:                  process.env.FRONTEND_URL,
+    port:                         process.env.PRODUCTION_API_PORT,
     mail: {
-        host:     process.env.MAIL_HOST || "--mail-host",
-        port:     process.env.MAIL_PORT || "--mail-port",
+        host:     process.env.MAIL_HOST,
+        port:     process.env.MAIL_PORT,
         secure:   booleanEnvVar(process.env.MAIL_IS_SECURE, true),
-        user:     process.env.MAIL_USER || "--mail-user",
-        pass:     process.env.MAIL_PASSWORD || "--mail-pass",
-        from:     process.env.MAIL_FROM_ADDRESS || "--mail-from"
+        user:     process.env.MAIL_USER,
+        pass:     process.env.MAIL_PASSWORD,
+        from:     process.env.MAIL_FROM_ADDRESS
     },
     db: {
-        host:             process.env.DB_HOST || "--db-host",
-        port:             process.env.DB_PORT || "--db-port",
-        name:             process.env.DB_NAME || "--db-name",
+        host:             process.env.DB_HOST,
+        port:             process.env.DB_PORT,
+        name:             process.env.DB_NAME,
         credentials: {
-            username:     process.env.DB_USERNAME || "--db-username",
-            password:     process.env.DB_PASSWORD || "--db-password"
+            username:     process.env.DB_USERNAME,
+            password:     process.env.DB_PASSWORD
         }
     },
     publicApi:{
-        port: process.env.PUBLIC_API_PORT || "--public-api-port"
+        port: process.env.PUBLIC_API_PORT
     },
-    adminMail: process.env.ADMIN_MAIL || "--admin-mail",
+    adminMail: process.env.ADMIN_MAIL,
     voiceReader:{
         properties:   [ "title", "brief", "description"],
-        baseUrl:      process.env.VOICE_READER_BASE_URL || "--voiceReader-baseUrl",
-        customerId:   process.env.VOICE_READER_CUSTOMER_ID || "--voiceReader-customerId"
+        baseUrl:      process.env.VOICE_READER_BASE_URL,
+        customerId:   process.env.VOICE_READER_CUSTOMER_ID
     },
     suggestions:{
-        apiKey:           process.env.SUGGESTIONS_API_KEY || "--suggestion-api-key",
+        apiKey:           process.env.SUGGESTIONS_API_KEY,
         requireMail:      booleanEnvVar(process.env.SUGGESTIONS_REQUIRE_MAIL, true),
-        requireApiKey:    process.env.SUGGESTIONS_REQUIRE_API_KEY || "new",
+        requireApiKey:    process.env.SUGGESTIONS_REQUIRE_API_KEY,
         requireName:      booleanEnvVar(process.env.SUGGESTIONS_REQUIRE_NAME, true),
         sendConfirmation: booleanEnvVar(process.env.SUGGESTIONS_SEND_CONFIRMATION, true)
     }
